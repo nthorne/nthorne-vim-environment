bc9e404a7e1b1a88b382a2f8d04afbae50b1c39b
Niklas Thorne <niklas.thorne@rail.bombardier.com>
2016-11-19 15:11:39 +0100

    Make my bash skeleton file more NixOS friendly.

--- skel/template.sh
+++ skel/template.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -
+#!/usr/bin/env bash
 
 # vim: filetype=sh
 
@@ -6,13 +6,23 @@
 IFS=' 	
 '
 
-# If found, use getconf to constructing a reasonable PATH, otherwise
-# we set it manually.
-if [[ -x /usr/bin/getconf ]]
+
+# Make sure that we're not on NixOS; if so, avoid tampering with PATH
+if [[ -f /etc/os-release ]]
 then
-  PATH=$(/usr/bin/getconf PATH)
-else
-  PATH=/bin:/usr/bin:/usr/local/bin
+  . /etc/os-release
+
+  if [[ "NixOS" != "$NAME" ]]
+  then
+    # If found, use getconf to constructing a reasonable PATH, otherwise
+    # we set it manually.
+    if [[ -x /usr/bin/getconf ]]
+    then
+      PATH=$(/usr/bin/getconf PATH)
+    else
+      PATH=/bin:/usr/bin:/usr/local/bin
+    fi
+  fi
 fi
 
 
